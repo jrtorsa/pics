f5651ab9ec405484ca590b3326966ebc5fece4f9
Raul T <jr.torsa@gmail.com>
2020-07-29 12:21:04 -0600

    adding image card and init ref to images

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,14 +4,22 @@ class ImageCard extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = { spans: 0 };
+
     this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans });
   }
 
   render() {
     const { description, urls } = this.props.image;
     return (
-      <div>
-        <img alt={description} src={urls.regular} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
       </div>
     );
   }
